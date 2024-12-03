8e57a074d85fb482313765b2c37d69ab23c478c1
pinkdahlia2001 <hopekombo@yahoo.co.uk>
2024-12-03 19:19:18 +0000

    exercises

---
+++ Working_with_lists/animals.py
@@ -0,0 +1,4 @@
+cats = ["cat", "tiger", "lion", "wildcat", "leopard", "cheetah", "jaguar"]
+for cat in cats:
+    print("A " + cat + " is the best type of cat!\n")
+print("However, some of these would not make suitable pets!")
