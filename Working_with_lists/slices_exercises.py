6342de6d2acb028e4b24aac84f18a6f4b4a0a35c
pinkdahlia2001 <hopekombo@yahoo.co.uk>
2024-12-04 17:19:24 +0000

    Slicing exercises

---
+++ Working_with_lists/slices_exercises.py
@@ -0,0 +1,11 @@
+locations = ['disney world', 'brazzaville', 'new york', 'provence', 'california', 'marseille', 'venice', 'seoul', 'tokyo']
+print(locations)
+
+print("The first three items in the list are: ")
+print(locations[:3])
+
+print("Three items from the middle of the list are:")
+print(locations [3:6])
+
+print("The last three items in the list are:")
+print(locations[-3:])
